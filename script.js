86aea2510f9accc8b8c808cc045d7b6ea2f91155
devlocalhost <lazarecobani@gmail.com>
2023-12-29 18:41:42 +0200

    fuck you

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var workingUrl = "https://laurel-updates-dev64.koyeb.app"
+var workingUrl = "https://laurel-updates-dev64.koyeb.app";
 //        main: https://laurel-updates.onrender.com
 // alternative: https://laurel-updates-dev64.koyeb.app
 var pathName = window.location.pathname;
 
 if (pathName !== "/") {
-    redirectLaurel = workingUrl + pathName
+    redirectLaurel = workingUrl + pathName;
 
     document.getElementById("redirectLink").href = redirectLaurel;
     window.location.href = redirectLaurel;
